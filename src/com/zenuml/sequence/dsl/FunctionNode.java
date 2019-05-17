4ad6b83a2f06a677cc2a7bfa842242f332e638cc
DengZhiGuo <bobdeng@163.com>
2019-05-17 14:10:34 +0800

    Default Changelist

--- src/com/zenuml/sequence/dsl/FunctionNode.java
+++ src/com/zenuml/sequence/dsl/FunctionNode.java
@@ -1,12 +1,17 @@
 package com.zenuml.sequence.dsl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FunctionNode implements DslNode {
     private final String className;
     private final String functionName;
+    private List<DslNode> children;
 
     public FunctionNode(String className, String functionName) {
         this.className = className;
         this.functionName = functionName;
+        this.children=new ArrayList<>();
     }
 
     @Override
