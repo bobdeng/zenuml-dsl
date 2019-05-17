dcacdc52568a5f4f9d73d5411bf3a7d9fd63a9aa
DengZhiGuo <bobdeng@163.com>
2019-05-17 14:36:44 +0800

    Default Changelist

--- src/com/zenuml/sequence/dsl/ConditionNode.java
+++ src/com/zenuml/sequence/dsl/ConditionNode.java
@@ -1,13 +1,15 @@
 package com.zenuml.sequence.dsl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConditionNode implements DslNode {
     private String condition;
     private List<DslNode> ifCondition;
-
+    private ConditionNode elseCondition;
     public ConditionNode(String condition) {
         this.condition = condition;
+        ifCondition = new ArrayList<>();
     }
 
     @Override
