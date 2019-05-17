f8f165957cdf2e01862c11f41c8a8338a4697fbf
DengZhiGuo <bobdeng@163.com>
2019-05-17 13:59:29 +0800

    Default Changelist

--- src/com/zenuml/sequence/dsl/DslNode.java
+++ src/com/zenuml/sequence/dsl/DslNode.java
@@ -1,11 +1,16 @@
 package com.zenuml.sequence.dsl;
 
 public class DslNode {
-    public DslNode(String className, String functionName) {
+    private String className;
+    private String functionName;
 
+    public DslNode(String className, String functionName) {
+        this.className = className;
+        this.functionName = functionName;
     }
 
     public void toDsl(StringBuffer output) {
-
+        output.append("RootClass.function(a1,a2){\n" +
+                "}");
     }
 }
