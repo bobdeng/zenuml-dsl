22926c0ae03966e81b5ce3208a9bac3c52bfbb2e
DengZhiGuo <bobdeng@163.com>
2019-05-17 14:04:29 +0800

    Default Changelist

--- test/com/zenuml/sequence/dsl/DslGeneratorTest.java
+++ test/com/zenuml/sequence/dsl/DslGeneratorTest.java
@@ -2,6 +2,8 @@ package com.zenuml.sequence.dsl;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class DslGeneratorTest {
 
     @Test
@@ -9,5 +11,7 @@ public class DslGeneratorTest {
         DslNode root=new FunctionNode("RootClass","function(a1,a2)");
         StringBuffer dsl=new StringBuffer();
         root.toDsl(dsl);
+        System.out.println(dsl.toString());
+        assertEquals("RootClass.function(a1,a2){\n}",dsl.toString());
     }
 }
