067a3c914fb43db588fc1bfa5117e3ceb4dff0cb
DengZhiGuo <bobdeng@163.com>
2019-05-19 14:11:39 +0800

    Default Changelist

--- src/com/zenuml/sequence/dsl/Loopnode.java
+++ src/com/zenuml/sequence/dsl/Loopnode.java
@@ -1,12 +1,15 @@
 package com.zenuml.sequence.dsl;
 
 public class Loopnode implements DslNode {
+    private String conditon;
+
     public Loopnode(String condition) {
+        this.conditon = condition;
     }
 
     @Override
     public void toDsl(StringBuffer output) {
-        
+
     }
 
     @Override
